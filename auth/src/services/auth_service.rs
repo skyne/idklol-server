f2294e1cb32a69657b650d24cab7f84503740fe7
MakaiAS <sandorattilamakai@gmail.com>
2025-11-27 14:52:14 +0100

    auth service logs with timestamp (chrono)

--- auth/src/services/auth_service.rs
+++ auth/src/services/auth_service.rs
@@ -1,9 +1,12 @@
 use tonic::{Request, Response, Status};
 use crate::auth::auth_service_server::AuthService;
 use crate::auth::{LoginRequest, LoginResponse, LoginStatus};
+use chrono::Local;
 
 #[derive(Debug, Default)]
 pub struct MyAuthService {}
+
+const  DATE_FORMAT_STRING: &'static str = "[%Y-%m-%d %H:%M:%S:%.3f]";
 
 #[tonic::async_trait]
 impl AuthService for MyAuthService {
@@ -11,7 +14,7 @@ impl AuthService for MyAuthService {
         &self,
         request: Request<LoginRequest>,
     ) -> Result<Response<LoginResponse>, Status> {
-        println!("Got a request: {:?}", request);
+        println!("{}: Got a request: {:?}", Local::now().format(DATE_FORMAT_STRING), request);
 
         let status = LoginStatus::Success;
 
