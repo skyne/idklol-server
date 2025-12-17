8409523212523d177fc9c652cb6417ba14319e7a
Daniel Heringei <daniel.heringei@incepteam.com>
2025-12-17 13:35:54 +0100

    chatserver

--- chat/src/main.rs
+++ chat/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world from chat!");
+use tonic::transport::Server;
+use chat::chat_service_server::ChatServiceServer;
+
+pub mod chat {
+    tonic::include_proto!("chat");
+}
+
+mod services;
+use services::chat_service::MyChatService;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:50052".parse()?;
+    let chat_service = MyChatService::default();
+
+    println!("Auth server running on {}", addr);
+
+    Server::builder()
+        .add_service(ChatServiceServer::new(chat_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
---
+++ chat/src/services/chat_service.rs
@@ -0,0 +1,68 @@
+use std::sync::Arc;
+use tonic::{Request, Response, Status};
+use crate::chat::chat_service_server::ChatService;
+use crate::chat::ChatMessage;
+use chrono::Local;
+use tokio::sync::{mpsc, Mutex};
+use tokio_stream::wrappers::ReceiverStream;
+
+#[derive(Debug)]
+pub struct MyChatService {
+    subscribers: Arc<Mutex<Vec<mpsc::Sender<Result<ChatMessage, Status>>>>>,
+}
+
+impl Default for MyChatService {
+    fn default() -> Self {
+        Self {
+            subscribers: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+}
+
+const  DATE_FORMAT_STRING: &'static str = "[%Y-%m-%d %H:%M:%S:%.3f]";
+
+#[tonic::async_trait]
+impl ChatService for MyChatService {
+    type StreamStream = ReceiverStream<Result<ChatMessage, Status>>;
+
+    async fn message(
+        &self,
+        request: Request<ChatMessage>,
+    ) -> Result<Response<()>, Status> {
+        println!(
+            "{}: Got a request: {:?}",
+            Local::now().format(DATE_FORMAT_STRING),
+            request
+        );
+
+        let msg = request.into_inner();
+
+        let subscribers = self.subscribers.lock().await;
+        for tx in subscribers.iter() {
+            // Ignore send errors (e.g., disconnected clients)
+            let _ = tx.send(Ok(msg.clone())).await;
+        }
+
+        Ok(Response::new(()))
+    }
+
+    async fn stream(
+        &self,
+        request: Request<()>,
+    ) -> Result<Response<Self::StreamStream>, Status> {
+        println!(
+            "{}: Got a request: {:?}",
+            Local::now().format(DATE_FORMAT_STRING),
+            request
+        );
+
+        let (tx, rx) = mpsc::channel(16);
+
+        {
+            let mut subscribers = self.subscribers.lock().await;
+            subscribers.push(tx);
+        }
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
+}
